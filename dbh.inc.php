6f5a4497d3f019615693110bd7b2936ed20301c0
Pushpit07 <57194612+Pushpit07@users.noreply.github.com>
2020-05-21 17:19:33 +0530

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "MyCapital";
+$servername = "us-cdbr-east-06.cleardb.net";
+$dbUsername = "b4f40b3662d6a7";
+$dbPassword = "4ee6b859";
+$dbName = "heroku_0ae366c24d3c517";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
